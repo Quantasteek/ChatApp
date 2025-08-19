bf1f498fbbea877ad0ac0a2a8c93ab027767f2d4
Quantasteek <139091681+Quantasteek@users.noreply.github.com>
2025-08-19 21:28:40 +0530

    Message model and controller

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,6 +3,7 @@ import authRoutes from "./routes/auth.route.js"
 import dotenv from "dotenv"
 import { connectDB } from "./lib/db.js";
 import cookieParser from 'cookie-parser';
+import messageRoutes from "./routes/message.route.js";
 
 const app =e();
 dotenv.config()
@@ -10,6 +11,7 @@ app.use(cookieParser());
 app.use(e.json())
 app.use('/api/auth', authRoutes)
 const port = process.env.PORT || 3001
+app.use('/api/message', messageRoutes);
 
 app.listen(port, ()=>{
     console.log(`Server listening on PORT ${port}`);
