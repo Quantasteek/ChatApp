b36be135e1b5432804e812eebce23abe98185d37
Quantasteek <139091681+Quantasteek@users.noreply.github.com>
2025-08-24 19:30:15 +0530

    profile pic upload functionality

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -76,25 +76,28 @@ export const logout = (req, res)=>{
     }
 }
 
-export const updateProfile= async (req, res) => {
-    try {
-        const {profilePic} = req.body;
-        const userId = req.user._id;
-        if(!profilePic) {
-            return res.status(400).json({message: "Profile picture is required"});
-        }
+export const updateProfile = async (req, res) => {
+  try {
+    const { profilePic } = req.body;
+    const userId = req.user._id;
 
-        await cloudinary.uploader.upload(profilePic);
-        const updatedUser = await User.findByIdAndUpdate(userId, { profilePic: uploadResponse.secure_url }, { new: true });
-        res.status(200).json({
-            message: "Profile updated successfully",
-            user: updatedUser
-        });
-    } catch (error) {
-        console.log("Error in updateProfile:", error);
-        res.status(500).json({ message: "Internal server error" });        
+    if (!profilePic) {
+      return res.status(400).json({ message: "Profile pic is required" });
     }
-}
+
+    const uploadResponse = await cloudinary.uploader.upload(profilePic);
+    const updatedUser = await User.findByIdAndUpdate(
+      userId,
+      { profilePic: uploadResponse.secure_url },
+      { new: true }
+    );
+
+    res.status(200).json(updatedUser);
+  } catch (error) {
+    console.log("error in update profile:", error);
+    res.status(500).json({ message: "Internal server error" });
+  }
+};
 
 export const checkAuth = (req, res) => {
     try {
--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -2,6 +2,7 @@ import {create} from "zustand"
 import { axiosInstance } from "../lib/axios"
 import toast from "react-hot-toast"
 
+
 export const useAuthStore = create((set) => ({
     authUser:null,
     isSigningUp:false,
@@ -53,6 +54,20 @@ export const useAuthStore = create((set) => ({
             toast.success("Logged out successfully")
         } catch (error) {
             toast.error(error.response.data.message)
+        }
+    },
+    updateProfile: async(data)=>{
+        set({isUpdatingProfile:true})
+        try {
+            const res = await axiosInstance.put('/auth/update-profile', data)
+            set({authUser:res.data})
+            toast.success("Profile updated successfully")
+        } catch (error) {
+            console.log(error);
+            toast.error(error.response?.data?.message || "Something went wrong")
+            
+        }finally{
+            set({isUpdatingProfile:false})
         }
     }
 }))
