e9dda1a946213213a82710b4b5b9c5788f4a014a
Quantasteek <139091681+Quantasteek@users.noreply.github.com>
2025-08-26 21:53:40 +0530

    home page implementation

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,11 +1,25 @@
 import React from 'react'
 import Navbar from '../components/Navbar'
+import Sidebar from '../components/Sidebar'
+import NoChatSelected from '../components/NoChatSelected'
+import ChatContainer from '../components/ChatContainer'
+import { useChatStore } from '../store/useChatStore'
 
 const HomePage = () => {
+  const {selectedUser}=useChatStore()
   return (
-    <div>
-        <Navbar/>
-      HomePage
+    <div className='h-screen bg-base-200 '>
+      <div className='flex items-center justify-center pt-20 px-4'>
+        <div className='bg-base-100 rounded-lg shadow-x-lg w-full max-w-6xl h-[calc(100vh-8rem)] '>
+          <div className='h-full flex rounded-lg overflow-hidden'>
+            <Sidebar/>
+            {!selectedUser ?<NoChatSelected/>: <ChatContainer/>}
+          </div>
+
+        </div>
+
+      </div>
+        
     </div>
   )
 }
