a225e7e80c49a6b76b3ac20f9559bf42ae72a0e9
Quantasteek <139091681+Quantasteek@users.noreply.github.com>
2025-08-27 12:54:05 +0530

    5001 removed to 3000

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,24 +1,42 @@
-import e from "express";
-import authRoutes from "./routes/auth.route.js"
-import dotenv from "dotenv"
+import express from "express";
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+
+import path from "path";
+
 import { connectDB } from "./lib/db.js";
-import cookieParser from 'cookie-parser';
-import cors from "cors"
+
+import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
-import {app, server} from "./lib/socket.js";
+import { app, server } from "./lib/socket.js";
 
+dotenv.config();
 
-dotenv.config()
+const PORT = process.env.PORT;
+const __dirname = path.resolve();
+
+app.use(express.json());
 app.use(cookieParser());
-app.use(e.json())
-app.use(cors({origin: "http://localhost:5173", credentials:true}))
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
 
-app.use('/api/auth', authRoutes)
-const port = process.env.PORT || 3001
-app.use('/api/messages', messageRoutes);
+app.use("/api/auth", authRoutes);
+app.use("/api/messages", messageRoutes);
 
-server.listen(port, ()=>{
-    console.log(`Server listening on PORT ${port}`);
-    connectDB()
-    
-})
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
+}
+
+server.listen(PORT, () => {
+  console.log("server is running on PORT:" + PORT);
+  connectDB();
+});
